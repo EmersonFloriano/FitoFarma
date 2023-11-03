bee3a2eb60500d007eca0b6f22a1665f58d44ce6
Dhiana Pereira <dhiana.pereira@unico.io>
2023-11-03 18:57:53 -0300

    refactor: Remove initial files

---
+++ app/src/main/java/br/com/emerson/fitofarma/activities/CatalogActivity.kt
@@ -0,0 +1,12 @@
+package br.com.emerson.fitofarma.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.emerson.fitofarma.R
+
+class CatalogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_catalog)
+    }
+}
